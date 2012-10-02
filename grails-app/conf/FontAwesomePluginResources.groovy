7fc086c63bda50da9ac84c56ef47690768be5d12
eamonnoconnell@gmail.com <eamonnoconnell@gmail.com>
2012-10-02 14:12:07 +0200

    Fixed bug in Resources file, defining plugin namespace.

--- grails-app/conf/FontAwesomePluginResources.groovy
+++ grails-app/conf/FontAwesomePluginResources.groovy
@@ -1,7 +1,7 @@
 modules = {
 
   'font-awesome' {
-    resource url:[dir: 'less', file: 'font-awesome.less'], attrs:[rel: "stylesheet/less", type:'css']
+    resource url:[plugin: 'font-awesome-resources', dir: 'less', file: 'font-awesome.less'], attrs:[rel: "stylesheet/less", type:'css']
   }
 
 }
