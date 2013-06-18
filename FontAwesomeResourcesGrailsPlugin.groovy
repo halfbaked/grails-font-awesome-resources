40c8373d1ac32c09b9f6cf65590808216d9135be
34m0 <eamonnoconnell@gmail.com>
2013-06-18 08:20:54 +0200

    upgraded to font-awesome 3.2.1

--- FontAwesomeResourcesGrailsPlugin.groovy
+++ FontAwesomeResourcesGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class FontAwesomeResourcesGrailsPlugin {
     // the plugin version
-    def version = "3.1"
+    def version = "3.2.1"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.0 > *"
     // the other plugins this plugin depends on
--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -22,7 +22,7 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-      build(":tomcat:$grailsVersion", ":release:2.2.0") {
+      build(":tomcat:$grailsVersion", ":release:2.2.0", ":rest-client-builder:1.0.3") {
           export = false
       }
       runtime ":resources:1.1.6"
