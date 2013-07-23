425634ea3815009b4a2fb9d9ec8f0d71df15841d
Rick Jensen <cdeszaq@gmail.com>
2013-07-22 21:01:02 -0500

    Non-dev environment will automatically switch to the minified version

--- grails-app/conf/FontAwesomePluginResources.groovy
+++ grails-app/conf/FontAwesomePluginResources.groovy
@@ -1,7 +1,7 @@
 modules = {
 
   'font-awesome' {
-    resource url:[plugin: 'font-awesome-resources', dir: 'css', file: 'font-awesome.css']
+    resource url: [plugin: 'font-awesome-resources', dir: 'css', file: (dev ? 'font-awesome.css' : 'font-awesome.min.css')], exclude: 'minify'
   }
 
 }
