7fb61562729b33e407d91275eb4ec416a86b300c
Eamonn O'Connell <eamonnoconnell@gmail.com>
2014-08-05 11:23:57 +0200

    version bump

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -19,7 +19,7 @@ grails.project.dependency.resolution = {
         build(':release:3.0.1', ':rest-client-builder:2.0.0') {
             export = false
         }
-        runtime(":resources:1.2.1") {
+        runtime(":resources:1.2.7") {
             export = false
         }
     }
