67ae6a996f6615c2686377267ce84f65d2feedf0
Soeren Berg Glasius <soeren@glasius.dk>
2014-08-21 21:18:32 -0700

    Fixes #29

--- grails-app/resourceMappers/FontAwesomePluginResourceMapper.groovy
+++ grails-app/resourceMappers/FontAwesomePluginResourceMapper.groovy
@@ -1,12 +1,9 @@
-import org.grails.plugin.resource.mapper.MapperPhase
-
-
-// Removes the warning seen in the Chrome debugger: 
+// Removes the warning seen in the Chrome debugger:
 // ‘Resource interpreted as Font but transferred with MIME type application/octet-stream’ 
 // http://kalikallin.tumblr.com/post/25182657116/setting-grails-content-type-for-a-given-file-extension
 class FontAwesomePluginResourceMapper {
-
-    static phase = MapperPhase.ALTERNATEREPRESENTATION
+    // This decouples the plugin from the resources plugin.
+    static phase = FontAwesomePluginResourceMapper.class.forName('org.grails.plugin.resource.mapper.MapperPhase').enumConstants.find { it.name() == "ALTERNATEREPRESENTATION" }
     static defaultIncludes = [ '**/*.woff' ]
 
     def map(resource, config) {
