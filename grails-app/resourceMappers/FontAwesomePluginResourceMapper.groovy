49e1e2677ba7454a0ed4820c96f4ffb1d6cdcb26
Soeren Berg Glasius <soeren@glasius.dk>
2014-09-04 20:19:02 -0700

    My first attempt to fix the resource mapper does in fact not work. This one seems to be working.

--- grails-app/resourceMappers/FontAwesomePluginResourceMapper.groovy
+++ grails-app/resourceMappers/FontAwesomePluginResourceMapper.groovy
@@ -2,8 +2,8 @@
 // ‘Resource interpreted as Font but transferred with MIME type application/octet-stream’ 
 // http://kalikallin.tumblr.com/post/25182657116/setting-grails-content-type-for-a-given-file-extension
 class FontAwesomePluginResourceMapper {
-    // This decouples the plugin from the resources plugin.
-    static phase = FontAwesomePluginResourceMapper.class.forName('org.grails.plugin.resource.mapper.MapperPhase').enumConstants.find { it.name() == "ALTERNATEREPRESENTATION" }
+    static phase = alternateRepresentationPhase
+
     static defaultIncludes = [ '**/*.woff' ]
 
     def map(resource, config) {
@@ -11,6 +11,16 @@ class FontAwesomePluginResourceMapper {
             resp.setHeader('Content-Type', 'font/opentype')
         }
     }
+
+    // This decouples the plugin from the resources plugin.
+    private static Object getAlternateRepresentationPhase() {
+        try {
+            FontAwesomePluginResourceMapper.class.forName('org.grails.plugin.resource.mapper.MapperPhase').enumConstants.find { it.name() == "ALTERNATEREPRESENTATION" }
+        } catch (ignored) {
+            null
+        }
+    }
+
 
 }
 
